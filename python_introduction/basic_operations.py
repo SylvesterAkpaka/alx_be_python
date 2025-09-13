5a960bd7286800e5bdd419b318ee209761547c4e
Sylvester Akpaka <sylvesterakpaka@gmail.com>
2025-09-13 18:53:40 +0000

    Move basic_operations.py to python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add_result = number1 + number2 
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {add_result}")
+print(f"Substraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
