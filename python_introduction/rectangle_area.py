5532d03bc3f4762f38f7573aad2cba4dc3f70cc0
Sylvester Akpaka <sylvesterakpaka@gmail.com>
2025-09-14 13:18:23 +0000

    Added rectangle_area.py to python_introduction directory

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+length = 10 #representing the length of the triangle
+width = 5 #representing the width of the triangle
+area = length * width #formula for calculating area of a triangle
+
+print(f"The area of the triangle is: {area}")
