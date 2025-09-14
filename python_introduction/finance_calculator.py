d6275ad4214db95f00b296615d0c961e9de81759
Sylvester Akpaka <sylvesterakpaka@gmail.com>
2025-09-14 17:14:42 +0000

    Update finance_calculator to python_introduction with new changes

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = float(input("Enter your monthly income: ")) #prompt the user for monthly income
-expenses = float(input("Enter your total monthly expenses: ")) #prompt the user for monthly expenses
+monthly_income = float(input("Enter your monthly income: ")) #prompt the user for monthly income
+monthly_expenses = float(input("Enter your total monthly expenses: ")) #prompt the user for monthly expenses
 
-monthly_savings = income - expenses #calculate monthly savings
+monthly_savings = monthly_income - monthly_expenses #calculate monthly savings
 interest_rate = 0.05 #annual interest rate = 5%
 annual_savings = (monthly_savings * 12) + (monthly_savings * 12 * interest_rate) #project savings after one year with simple interest
 
