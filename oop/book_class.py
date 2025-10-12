031044d525bba2c5c879773907d10e22ffb39aac
Sylvester Akpaka <sylvesterakpaka@gmail.com>
2025-10-12 17:47:06 +0000

    Add book_class.py to oop directory

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+class Book:
+    """A class to represent a book using Python magic methods."""
+
+    def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that prints a message when the object is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Human-readable string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official string representation that can recreate the object."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
